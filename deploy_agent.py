4ff231be0608c51cd01aefde131edad6dfaf8f81
Adrian Albu <2412rock@gmail.com>
2023-11-05 15:43:30 +0200

    Final form

--- deploy_agent.py
+++ deploy_agent.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import os
 import subprocess
+import shutil
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def post_data():
     # Get the JSON data from the request
     data = request.get_json()
-    f = open('C:/Users/Adi/Documents/password.txt', 'r')
+    f = open('C:/Users/Server/Documents/password.txt', 'r')
     password = f.readline()
     if password == data["password"]:
         deploy_fe = data["deploy_frontend"]
@@ -26,22 +27,24 @@ def post_data():
 
 
 def deploy_frontend():
-    os.chdir("C:/Users/Adi/Desktop")
+    os.chdir("C:/Users/Server/Desktop")
+    os.system("rmdir /S /Q Dorel-angular")
     os.system("git clone https://github.com/2412rock/Dorel-angular")
-    os.chdir("C:/Users/Adi/Desktop/Dorel-angular")
+    os.chdir("C:/Users/Server/Desktop/Dorel-angular")
     os.system("docker stop angular-app")
     os.system("docker rm angular-app")
     os.system("docker build -t dorel-angular .")
     subprocess.Popen(["docker", "run", "--name", "angular-app", "-p", "80:80", "dorel-angular"])
 
 def deploy_backend():
-    os.chdir("C:/Users/Adi/Desktop")
+    os.chdir("C:/Users/Server/Desktop")
+    os.system("rmdir /S /Q Dorel-backend")
     os.system("git clone https://github.com/2412rock/Dorel-backend")
-    os.chdir("C:/Users/Adi/Desktop/Dorel-backend")
+    os.chdir("C:/Users/Server/Desktop/Dorel-backend")
     os.system("docker stop dorel-backend")
     os.system("docker rm dorel-backend")
     os.system("docker build -t dorel-backend .")
     subprocess.Popen(["docker", "run" ,"--name" ,"dorel-backend", "-p" ,"4200:4200" ,"dorel-backend"])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="192.168.1.239", port="4300",debug=True)
