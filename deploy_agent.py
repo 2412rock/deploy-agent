9cd71c14ca2b87ee11027a0d8083956fac2a3027
Adrian Albu <2412rock@gmail.com>
2023-11-14 20:16:47 +0200

    Deploy redis and sql

--- deploy_agent.py
+++ deploy_agent.py
@@ -17,25 +17,45 @@ def post_data():
     if password == data["password"]:
         deploy_fe = data["deploy_frontend"]
         deploy_be = data["deploy_backend"]
+        should_deploy_redis = data["deploy_redis"]
+        should_deploy_sql = data["deploy_sql"]
 
         if deploy_fe:
             deploy_frontend()
         if deploy_be:
             deploy_backend()
+        if should_deploy_redis:
+            deploy_redis_server()
+        if should_deploy_sql:
+            deploy_sql_server()
 
         # Return a JSON response
         return jsonify("ok"), 200
     return jsonify("Bad password"), 400
 
-def get_local_ip():
-    # Create a socket and connect to an external server (e.g., Google's public DNS)
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    local_ip = s.getsockname()[0]
-    s.close()
-    return local_ip
+def get_redis_password():
+    file = open('C:/Users/Server/Documents/redis_password.txt', 'r')
+    redis_password = file.readline()
+    file.close()
+    return redis_password
 
+def deploy_redis_server():
+    os.chdir("C:/Users/Server/Desktop")
+    os.system("rmdir /S /Q redis-docker")
+    os.system("git clone https://github.com/2412rock/redis-docker")
+    os.chdir("C:/Users/Server/Desktop/redis-docker")
+    os.system("docker stop redis-server")
+    os.system("docker rm redis-server")
+    os.system("docker build -t redis .")
     
+    subprocess.Popen(["docker", "run", "-d", "-p", "6379:6379", "--name", "redis-server", "-e", f"REDIS_PASSWORD={get_redis_password()}", "redis"])
+
+def getSqlPassword():
+    f = open('C:/Users/Server/Documents/sql_password.txt', 'r')
+    password = f.readline()
+    f.close()
+    return password
+
 def deploy_sql_server():
     os.chdir("C:/Users/Server/Desktop")
     os.system("rmdir /S /Q sql-server-docker")
@@ -44,7 +64,8 @@ def deploy_sql_server():
     os.system("docker stop sql-server")
     os.system("docker rm sql-server")
     os.system("docker build -t sql-server .")
-    subprocess.Popen(["docker", "run", "-d", "-p", "1433:1433", "--name", "sql-server"])
+    
+    subprocess.Popen(["docker", "run", "-e", f'SA_PASSWORD="{getSqlPassword()}"',"-d", "-p", "1433:1433", "--name", "sql-server"])
 
 
 def deploy_frontend():
@@ -70,8 +91,8 @@ def deploy_backend():
     email_password_file = open('C:/Users/Server/Documents/email_password.txt', 'r')
     email_password = email_password_file.readline()
     email_password_file.close()
-    subprocess.Popen(["docker", "run" , "-e", f'EMAIL_PASSWD="{email_password}"', f'HOST_IP="{get_local_ip()}"',"--name" ,"dorel-backend", "-p" ,"4200:4200" ,"dorel-backend"])
+    subprocess.Popen(["docker", "run" , "-e", f'EMAIL_PASSWD="{email_password}"', "-e", f'SA_PASSWORD="{getSqlPassword()}"',
+                       "-e", f'REDIS_PASSWORD="{get_redis_password()}"', "--name" ,"dorel-backend", "-p" ,"4200:4200" ,"dorel-backend"])
 
 if __name__ == '__main__':
-    #app.run(host="192.168.1.239", port="4300",debug=True)
-    print(get_local_ip())
+    app.run(host="192.168.1.239", port="4300",debug=True)
