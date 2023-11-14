eb9030d3fd9afe7a6122aa25e9f25cd9a0ed3ad0
Adrian Albu <2412rock@gmail.com>
2023-11-14 19:32:24 +0200

    Add sql server ip

--- deploy_agent.py
+++ deploy_agent.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import shutil
 import glob
+import socket
 
 app = Flask(__name__)
 
@@ -26,6 +27,24 @@ def post_data():
         return jsonify("ok"), 200
     return jsonify("Bad password"), 400
 
+def get_local_ip():
+    # Create a socket and connect to an external server (e.g., Google's public DNS)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+    s.close()
+    return local_ip
+
+    
+def deploy_sql_server():
+    os.chdir("C:/Users/Server/Desktop")
+    os.system("rmdir /S /Q sql-server-docker")
+    os.system("git clone https://github.com/2412rock/sql-server-docker")
+    os.chdir("C:/Users/Server/Desktop/sql-server-docker")
+    os.system("docker stop sql-server")
+    os.system("docker rm sql-server")
+    os.system("docker build -t sql-server .")
+    subprocess.Popen(["docker", "run", "-d", "-p", "1433:1433", "--name", "sql-server"])
 
 
 def deploy_frontend():
@@ -51,7 +70,8 @@ def deploy_backend():
     email_password_file = open('C:/Users/Server/Documents/email_password.txt', 'r')
     email_password = email_password_file.readline()
     email_password_file.close()
-    subprocess.Popen(["docker", "run" , "-e", f'EMAIL_PASSWD="{email_password}"', "--name" ,"dorel-backend", "-p" ,"4200:4200" ,"dorel-backend"])
+    subprocess.Popen(["docker", "run" , "-e", f'EMAIL_PASSWD="{email_password}"', f'HOST_IP="{get_local_ip()}"',"--name" ,"dorel-backend", "-p" ,"4200:4200" ,"dorel-backend"])
 
 if __name__ == '__main__':
-    app.run(host="192.168.1.239", port="4300",debug=True)
+    #app.run(host="192.168.1.239", port="4300",debug=True)
+    print(get_local_ip())
