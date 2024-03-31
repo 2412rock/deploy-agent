ff89d97c36d5e3d9b8147ec318573e6eec71e63e
Adrian Albu <2412rock@gmail.com>
2024-03-31 23:04:30 +0300

    Fix sql migration

--- deploy_agent.py
+++ deploy_agent.py
@@ -79,9 +79,7 @@ def deploy_sql_migrations():
     os.system("rmdir /S /Q sql-server-docker")
     os.system("git clone https://github.com/2412rock/sql-server-docker")
     os.chdir("C:/Users/Server/Desktop/sql-server-docker")
-    subprocess.Popen(["docker", "run", "-e", f'SA_PASSWORD={getSqlPassword()}',"-d", "-p", "1433:1433", "--name", "sql-server", "sql-server"])
-    print('Waiting for server to start')
-    time.sleep(10)
+
     os.system("docker cp init.sql sql-server:/usr/src")
     os.system("docker cp pupulate_with_data.sql sql-server:/usr/src")
     #docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyP@ssword1! -d master -i /usr/src/init.sql
