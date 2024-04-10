73a6d89913542f4eb7c6c278bc6fb62a918d9aea
Adrian Albu <2412rock@gmail.com>
2024-04-11 00:14:04 +0300

    ip

--- deploy_agent_linux.py
+++ deploy_agent_linux.py
@@ -97,7 +97,7 @@ def deploy_sql_server():
     #docker run, -e, SA_PASSWORD=MyP@ssword1!,-d -p 1433:1433 --name sql-server sql-server
     subprocess.Popen([
     "docker", "run", "-e", f'SA_PASSWORD={getSqlPassword()}',
-    "-d", "-p", "192.168.1.100:1433:1433", "--name", "sql-server", "sql-server"
+    "-d", "-p", "10.132.0.2:1433:1433", "--name", "sql-server", "sql-server"
 ])
     print('Waiting for server to start')
     time.sleep(10)
